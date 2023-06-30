63d1e7df54c7098df7a25ee4ba7f48295b3a718c
Javier Tejero <ja.tejeror@gmail.com>
2023-06-30 07:25:56 +0200

    last but not least i wanna thank me

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,25 +1,45 @@
-import pandas as pd
-import pickle
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-# Cargar el modelo entrenado desde el archivo pickle
-with open('../models/trained_model.pkl', 'rb') as archivo_entrada:
-    model = pickle.load(archivo_entrada)
+# Hacer predicciones en los datos de prueba con cada modelo
+y_pred_svc1_model = svc1_model.predict(X_test)
+y_pred_gb1_model = gb1_model.predict(X_test)
+y_pred_kmeans4_model = kmeans4_model.predict(X_test)
 
-# Cargar los datos de prueba
-test_df = pd.read_csv('../data/test/test_df.csv')
-X_test = test_df.drop('DELAYED', axis=1)
-y_test = test_df['DELAYED']
+# Calcular las métricas de rendimiento para cada modelo
+# f1_svc1
+accuracy_svc1 = accuracy_score(y_test, y_pred_svc1_model)
+precision_svc1 = precision_score(y_test, y_pred_svc1_model, average='weighted')
+recall_svc1 = recall_score(y_test, y_pred_svc1_model, average='weighted')
+f1_svc1 = f1_score(y_test, y_pred_svc1_model, average='weighted')
 
-# Realizar las predicciones utilizando el modelo
-predictions = model.predict(X_test)
+# accuracy_gb1
+accuracy_gb1 = accuracy_score(y_test, y_pred_gb1_model)
+precision_gb1 = precision_score(y_test, y_pred_gb1_model, average='weighted')
+recall_gb1 = recall_score(y_test, y_pred_gb1_model, average='weighted')
+f1_gb1 = f1_score(y_test, y_pred_gb1_model, average='weighted')
 
-# Calcular la matriz de confusión
-confusion_matrix = confusion_matrix(y_test, predictions)
-print("Matriz de Confusión:")
-print(confusion_matrix)
+# accuracy_kmeans4
+accuracy_kmeans4 = accuracy_score(y_test, y_pred_kmeans4_model)
+precision_kmeans4 = precision_score(
+    y_test, y_pred_kmeans4_model, average='weighted')
+recall_kmeans4 = recall_score(y_test, y_pred_kmeans4_model, average='weighted')
+f1_kmeans4 = f1_score(y_test, y_pred_kmeans4_model, average='weighted')
 
-# Calcular el informe de clasificación
-classification_report = classification_report(y_test, predictions)
-print("Informe de Clasificación:")
-print(classification_report)
+# Imprimir las métricas de rendimiento para cada modelo
+print('Métricas de rendimiento para svc1_model:')
+print(f'Accuracy: {accuracy_svc1:.2f}')
+print(f'Precision: {precision_svc1:.2f}')
+print(f'Recall: {recall_svc1:.2f}')
+print(f'F1-score: {f1_svc1:.2f}')
+
+print('Métricas de rendimiento para gb1_model:')
+print(f'Accuracy: {accuracy_gb1:.2f}')
+print(f'Precision: {precision_gb1:.2f}')
+print(f'Recall: {recall_gb1:.2f}')
+print(f'F1-score: {f1_gb1:.2f}')
+
+print('Métricas de rendimiento para kmeans4_model:')
+print(f'Accuracy: {accuracy_kmeans4:.2f}')
+print(f'Precision: {precision_kmeans4:.2f}')
+print(f'Recall: {recall_kmeans4:.2f}')
+print(f'F1-score: {f1_kmeans4:.2f}')
